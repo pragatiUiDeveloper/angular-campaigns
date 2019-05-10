797be1b83a23b717b4df425bf07042407ffccb09
pragati <pragsjainprags@gmail.com>
2019-05-10 14:29:48 +0530

    updates to deploy

--- src/app/campaignService.service.ts
+++ src/app/campaignService.service.ts
@@ -8,6 +8,8 @@ export class CampaignService {
     constructor(private http: HttpClient) {}
     configUrl = 'http://demo0316771.mockable.io/campaigns';
     getCampaigns(){
-        return this.http.get(this.configUrl);
+      //  return this.http.get(this.configUrl);
+      return this.http.get("./assets/mydata.json");
+      
     }
 }
